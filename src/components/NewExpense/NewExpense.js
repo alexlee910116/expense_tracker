02843c4511178dd099d966bd1406ac483758407b
alexlee0116 <alexlee910116@gmail.com>
2023-05-11 23:32:06 +0900

    feat:chartbar

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,34 @@
-import './NewExpense.css';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditting, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-    ...enteredExpenseData,
-    id: Math.random().toString()
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
   };
-  props.onSaveExpenseData(expenseData)
-};
 
-  return <div className='new-expense'>
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-  </div>
+  const startEdtingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false)
+  }
+
+  return (
+    <div className='new-expense'>
+      {!isEditting && <button onClick={startEdtingHandler}>Add New Expense</button>}
+      {isEditting && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
+    </div>
+  );
 };
 
 export default NewExpense;
